b0fe369f7dd966fa3eafdec86482b0dadd5a830a
vickomaris <vicko.maris.s@gmail.com>
2022-08-28 23:10:39 +0700

    perbaikan jawaban no 4

--- NO1234.js
+++ NO1234.js
@@ -128,15 +128,22 @@ console.log(printData);
 //SOAL NO 4
 let harga= 25000;
 let jmlItem= 3;
-let discount= 0;
+let potongan=0;
 let totalHarga= harga*jmlItem;
+let discount = (totalHarga * 35 / 100);
+    if(discount > 50000){
+        discount = 50000
+    };
 
-if (totalHarga >= 60000) {
-    potongan = ((35 / 100) * totalHarga);
-    subTotal = (totalHarga - potongan);
-    } else {
-    potongan = ((35 / 100) * 0);
-    subTotal = (totalHarga - potongan);
+if(totalHarga >= 60000){
+        potongan = (discount);
+        //potongan = ((35 / 100) * totalHarga);
+        subTotal = (totalHarga - potongan);
+    }
+    else {
+        potongan = (discount*0);
+        //potongan = ((35 / 100) * 0);
+        subTotal = (totalHarga - potongan);
     }
 console.log("Total Harga : ",totalHarga);
 console.log("potongan", potongan);
